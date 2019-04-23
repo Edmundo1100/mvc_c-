67577e6542235826c2f758e16215000cbf7200c2
Edmundo <edmundo1100@hotmail.com>
2019-04-23 19:05:44 -0300

    Cadastro Vendedor 17 -236

--- SalesWebMvc/Controllers/VendedoresController.cs
+++ SalesWebMvc/Controllers/VendedoresController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Servicos;
 
 namespace SalesWebMvc.Controllers
@@ -20,6 +21,19 @@ namespace SalesWebMvc.Controllers
         {
             var list = _vendedorServico.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Vendedor vendedor)
+        {
+            _vendedorServico.Insert(vendedor);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
