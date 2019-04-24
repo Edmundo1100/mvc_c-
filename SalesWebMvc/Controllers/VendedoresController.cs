0228a9e4b2d3099ee6921c8812040c5441692b88
Edmundo <edmundo1100@hotmail.com>
2019-04-24 20:58:29 -0300

    Detalhar e carregar outras tabelas do objeto - 17-240

--- SalesWebMvc/Controllers/VendedoresController.cs
+++ SalesWebMvc/Controllers/VendedoresController.cs
@@ -62,5 +62,19 @@ namespace SalesWebMvc.Controllers
             _vendedorServico.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Detalhes(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _vendedorServico.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
     }
 }
--- SalesWebMvc/Servicos/VendedorServico.cs
+++ SalesWebMvc/Servicos/VendedorServico.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Servicos
 {
@@ -28,7 +29,7 @@ namespace SalesWebMvc.Servicos
 
         public Vendedor FindById(int id)
         {
-            return _context.Vendedor.FirstOrDefault(obj => obj.Id == id);
+            return _context.Vendedor.Include(obj => obj.Departamento).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
